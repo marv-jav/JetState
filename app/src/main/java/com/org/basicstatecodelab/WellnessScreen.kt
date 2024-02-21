de3f2b45d3323931470fbfc42a7e7ce6199072cc
Ayoola Marvellous <ayoolamarvellous83@gmail.com>
2024-02-21 10:23:45 +0100

    Refactored the code in normal archi patterns

--- app/src/main/java/com/org/basicstatecodelab/WellnessScreen.kt
+++ app/src/main/java/com/org/basicstatecodelab/WellnessScreen.kt
@@ -1,2 +1,21 @@
 package com.org.basicstatecodelab
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+@Composable
+fun WellnessScreen(
+    modifier: Modifier = Modifier,
+    wellnessViewModel: WellnessViewModel = viewModel()
+) {
+    Column(modifier = modifier) {
+        StatefulCounter(modifier)
+
+        WellnessTasksList(
+            list = wellnessViewModel.tasks,
+            onCloseTask = { task -> wellnessViewModel.removeItem(task) },
+            onCheckedTask = { task, checked -> wellnessViewModel.changeTaskChecked(task, checked) })
+    }
+}
